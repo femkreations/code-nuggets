71c3e2ceff1f9acc8aa692de4a45a2436c1208e8
femy praseeth <femkreations@gmail.com>
2017-04-24 12:28:22 -0700

    script js

--- functions/script.js
+++ functions/script.js
@@ -1,3 +1,10 @@
 // JavaScript Document
+function speakSomething(what, howMany){
+	var  what = (typeof what !== undefined ) ? what : 'Default Speech';
+	var  howMany = (typeof howMany !== undefined ) ? what : 'Default Speech';
 
+
+
+
+}
 
